2f15b8004ed91a4da67140c6c4ce899aa39ddce8
dustine32 <dustine32@gmail.com>
2020-09-21 15:37:58 -0700

    Hook up issue query

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh -l
 
 python3 --version
-py_out=$(python3 test_main.py $1)
-echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+py_out=$(python3 main.py $1 $2)
+
 echo "::set-output name=py_out::$py_out"
--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from github import Github
 import re
+import requests
+import json
+import datetime
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('repo_name')
+parser.add_argument('duration_in_days')
 
 
 # TODO: Confirm this is working
@@ -8,12 +15,12 @@ def make_html_safe(s):
     s = s.replace("&", '&amp;')
     s = s.replace("\"", '&quot;')
     s = s.replace(">", '&gt;')
-    s = s.replace("<", '&lt;');
+    s = s.replace("<", '&lt;')
     return s
 
 
 def print_single_issue(issue):
-    line = f"<li><a href=\"{issue.html_url}\">{issue.number}</a> {make_html_safe(issue.title)}</li>"
+    line = f"<li><a href=\"{issue['html_url']}\">{issue['number']}</a> {make_html_safe(issue['title'])}</li>"
     print(line)
 
 
@@ -21,12 +28,13 @@ def print_issues(issues, event_type: str, printed_ids: set):
     to_prints = []
     printed_count = 0
     for issue in issues:
-        if issue.number not in printed_ids:
+        if issue["number"] not in printed_ids:
             to_prints.append(issue)
             printed_count += 1
-            printed_ids.add(issue.number)
+            printed_ids.add(issue["number"])
+    print(f"<h3>{event_type} Tickets</h3>")
     if printed_count > 0:
-        print(f"There are {printed_count} {event_type} tickets.")
+        print(f"There are {printed_count} {event_type.lower()} tickets.")
         print("<ul>")
         [print_single_issue(i) for i in to_prints]
         print("</ul>")
@@ -34,16 +42,28 @@ def print_issues(issues, event_type: str, printed_ids: set):
         print(f"<p>There have been no {event_type.lower()} tickets.</p>")
 
 
-g = Github()
+def get_issues(repo: str, event_type: str, start_date: str):
+    url = f"https://api.github.com/search/issues?q=repo:{repo}+{event_type}:=>{start_date}&type=Issues&per_page=100"
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        resp_objs = json.loads(resp.content)
+        issues = resp_objs.get("items", [])
+        return issues
+    else:
+        raise Exception(f"HTTP error status code: {resp.status_code} for url: {url}")
 
-repo = "geneontology/go-ontology"
-# repo = "geneontology/amigo"
-start_date = "2020-06-18"
-new_issues = g.search_issues(query=f"repo:{repo} created:=>{start_date}")
-# print(new_issues.totalCount)
-updated_issues = g.search_issues(query=f"repo:{repo} updated:=>{start_date}")
-# print(updated_issues.totalCount)
 
-ids = set()
-print_issues(new_issues, "New", ids)
-print_issues(updated_issues, "Updated", ids)
+if __name__ == "__main__":
+    # repo = "geneontology/go-ontology"
+    # repo = "geneontology/amigo"
+    args = parser.parse_args()
+    today = datetime.date.today()
+    yesterday = today - datetime.timedelta(int(args.duration_in_days))
+
+    new_issues = get_issues(args.repo_name, "created", yesterday)
+    updated_issues = get_issues(args.repo_name, "updated", yesterday)
+
+    print(f"<h2>Summary for tickets from {yesterday} to {today}</h2>")
+    ids = set()
+    print_issues(new_issues, "New", ids)
+    print_issues(updated_issues, "Updated", ids)
