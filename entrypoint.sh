170b4b1654cac46705baba2b8212148e6f3ff108
dustine32 <dustine32@gmail.com>
2020-09-21 16:35:53 -0700

    Try quoting the echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ python3 -m venv env
 . env/bin/activate
 pip install -r requirements.txt
 
-issue_email_body=$(python3 main.py $1 $2 | sed 's/\n/%0A/g')
+issue_email_body=$(python3 main.py $1 $2)
 
 echo "::set-output name=issue_email_body::$issue_email_body"
