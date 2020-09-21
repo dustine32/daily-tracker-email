12f8c3f0f3081924ef2a190f6b56d243a520eaa6
dustine32 <dustine32@gmail.com>
2020-09-21 16:43:41 -0700

    Move to bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 python3 --version
 python3 -m venv env
@@ -6,6 +6,6 @@ python3 -m venv env
 pip install -r requirements.txt
 
 issue_email_body=$(python3 main.py $1 $2)
-wc <<<$issue_email_body
+wc <<< $issue_email_body
 
 echo "::set-output name=issue_email_body::$issue_email_body"
