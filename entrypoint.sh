03d70e152253fe0d6ace51750e74b0d5d1132fdb
dustine32 <dustine32@gmail.com>
2020-09-21 16:19:58 -0700

    Escape newlines

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ python3 -m venv env
 pip install -r requirements.txt
 
 issue_email_body=$(python3 main.py $1 $2)
+issue_email_body=$(cat $issue_email_body | sed 's/\n/%0A/g')
 
 echo "::set-output name=issue_email_body::$issue_email_body"
